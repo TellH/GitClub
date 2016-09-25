8d33cc6f5d50df6fcb56b8ee2535ba10851aea92
TellH <570495627@qq.com>
2016-09-25 17:23:48 +0800

    init share config

--- app/src/main/java/tellh/com/gitclub/common/AndroidApplication.java
+++ app/src/main/java/tellh/com/gitclub/common/AndroidApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
+import com.umeng.socialize.PlatformConfig;
 
 import tellh.com.gitclub.common.utils.LogUtils;
 import tellh.com.gitclub.di.component.AppComponent;
@@ -35,6 +36,11 @@ public class AndroidApplication extends Application {
         Stetho.initializeWithDefaults(this);
         refWatcher = LeakCanary.install(this);
         appComponent = DaggerAppComponent.builder().build();
+
+        //ShareSDk
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
     }
 
     public RefWatcher getRefWatcher() {
---
+++ app/src/main/java/tellh/com/gitclub/wxapi/WXEntryActivity.java
@@ -0,0 +1,9 @@
+package tellh.com.gitclub.wxapi;
+
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+}
